9c7dd20bd288df9051c1f287a8bd0f7999df7624
hun <rlagns_-@naver.com>
2019-03-07 20:18:26 +0900

    debug

--- op_project/settings.py
+++ op_project/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'u0q--q(x+(mn__idy@j2&e30m)=ip)crd(9l=c47&^07&-h_wi'
-import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+# import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'u0q--q(x+(mn__idy@j2&e30m)=ip)crd(9l=c47&^07&-h_wi')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
